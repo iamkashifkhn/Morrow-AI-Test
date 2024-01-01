347b63284648ceda898ba7bad96175832ce2d4a4
iamkashifkhn <usmankashif544@gmail.com>
2024-01-01 14:03:25 +0500

    bug fixing

--- src/Pages/AllCharts/apex/BusiestHour.js
+++ src/Pages/AllCharts/apex/BusiestHour.js
@@ -1,127 +1,3 @@
-// import React from "react";
-// import Chart from "react-apexcharts";
-// import { Spinner } from "reactstrap";
-
-// const HeatmapChart = ({data, loading}) => {
-//   if (!data) {
-//     return (
-//       <div className="page-content" style={{ display:'flex', justifyContent:'center'}}>
-//       <Spinner className="m-5" color="primary">
-//         Loading...
-//       </Spinner>
-//       </div>
-//     );
-//   }
-
-//   const chartData = {
-//     series: [
-//       {
-//         name: "Visitors",
-//         data: data?.map((entry) => ({
-//           x: `${entry.hour * 2} Hrs`,
-//           // y: entry.total_average_visitors * Math.floor(Math.random() * 20),
-//           y: Math.abs(entry.total_average_visitors * Math.floor(Math.random() * 20)),
-//         })),
-//       },
-//     ],
-//     options: {
-//       chart: {
-//         type: "heatmap",
-//       },
-//       xaxis: {
-//         type: "category",
-//         title: { text: "2 Hours Interval" },
-//       },
-//     },
-//   };
-
-//   // if (loading) {
-//   //   return (
-//   //     <div className="page-content" style={{ display:'flex', justifyContent:'center'}}>
-//   //     <Spinner className="m-5" color="primary">
-//   //       Loading...
-//   //     </Spinner>
-//   //     </div>
-//   //   );
-//   // }
-
-
-//   return (
-//     <div>
-//       <Chart
-//         options={chartData.options}
-//         series={chartData.series}
-//         type="heatmap"
-//         height={350}
-//       />
-//     </div>
-//   );
-// };
-
-// export default HeatmapChart;
-
-// import React from 'react';
-// import Chart from 'react-apexcharts';
-// import { Spinner } from 'reactstrap';
-
-// const HeatmapChart = ({ data }) => {
-//   if (!data) {
-//     return (
-//       <div className="page-content" style={{ display:'flex', justifyContent:'center'}}>
-//       <Spinner className="m-5" color="primary">
-//         Loading...
-//       </Spinner>
-//       </div>
-//     );
-//   }
-  
-
-//   const options = {
-//     chart: {
-//       type: 'heatmap',
-//     },
-//     dataLabels: {
-//       enabled: false,
-//     },
-//     colors: ['#008FFB'],
-//     xaxis: {
-//       type: 'category',
-//       title : {
-//         text: "Hours"
-//       }
-//       // categories: Object?.keys(data),
-//     },
-//     yaxis: {
-//       title: {
-//         text: 'Date',
-//         // categories: Object.keys(data[Object.keys(data)[0]]).map(entry => entry.hour),
-//       },
-//     },
-//     plotOptions: {
-//       heatmap: {
-//         radius: 30,
-//       },
-//     },
-//   };
-
-//   const series = Object.keys(data).map((date) => ({
-//     name: date,
-//     data: data[date].map((entry) => ({
-//       x: entry.hour,
-//       y: entry.visits,
-//       // value: entry.visits,
-//     })).slice(1),
-//   }));
-
-//   return (
-//     <div>
-//       <Chart options={options} series={series} type="heatmap" height="400" />
-//     </div>
-//   );
-// };
-
-// export default HeatmapChart;
-
 import React from 'react';
 import Chart from 'react-apexcharts';
 import { Spinner } from 'reactstrap';
@@ -139,7 +15,14 @@ const HeatmapChart = ({ data }) => {
 
   // Remove the first index from the x-axis categories and y-axis data
   const categories = Object.keys(data).slice(1);
-  const yData = Object.keys(data[categories[0]]).map((entry) => entry.hour).slice(1);
+  // const yData = Object.keys(data[categories[0]]).map((entry) => entry.hour).slice(1);
+  const xData = categories.map((date) =>
+  data[date].map((entry) => entry.hour)
+);
+
+// const flattenedXData = [].concat(...xData);
+
+// console.log(xData)
 
   const options = {
     chart: {
@@ -154,13 +37,13 @@ const HeatmapChart = ({ data }) => {
       title: {
         text: 'Hours',
       },
-      categories: categories,
+      categories: xData[0],
     },
     yaxis: {
       title: {
         text: 'Date',
       },
-      categories: yData,
+      categories: categories,
     },
     plotOptions: {
       heatmap: {
@@ -173,7 +56,7 @@ const HeatmapChart = ({ data }) => {
   const series = categories.map((date) => ({
     name: date,
     data: data[date].map((entry) => ({
-      x: entry.hour,
+      x: xData[0],
       y: `${entry.visits} visits`,
     })).slice(1),
   }));
